003108090d16e5c7e66427a0121bb1df54d2ca88
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2025-07-15 14:57:53 +1200

    Still no damage

--- deliveryquest/addons/cogito/CogitoObjects/cogito_projectile.gd
+++ deliveryquest/addons/cogito/CogitoObjects/cogito_projectile.gd
@@ -6,7 +6,7 @@ class_name CogitoProjectile
 # Lifespan is being set by the Lifespan timer.
 @onready var lifespan = $Lifespan
 # Damage is being set by the wieldable.
-var damage_amount : int = 10
+var damage_amount : int = 0
 
 ## Determine what happens if the projectile hits something. Keep this false for stuff like Arrows. True for stuff like bullets or rockets.
 @export var destroy_on_impact : bool = false
